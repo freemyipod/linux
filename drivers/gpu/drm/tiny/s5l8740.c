a5f613dd3a27fbf67f5a59b0b92eb02e1f9e2f36
Vencislav Atanasov <user890104@freemyipod.org>
2026-05-14 03:16:36 +0300

    s5l8740-lcdif: Add missing include for drm_dbg()

--- drivers/gpu/drm/tiny/s5l8740.c
+++ drivers/gpu/drm/tiny/s5l8740.c
@@ -20,6 +20,7 @@
 #include <drm/drm_fb_dma_helper.h>
 #include <drm/drm_gem_dma_helper.h>
 #include <drm/drm_modeset_helper_vtables.h>
+#include <drm/drm_print.h>
 #include <drm/drm_probe_helper.h>
 #include <drm/clients/drm_client_setup.h>
 
